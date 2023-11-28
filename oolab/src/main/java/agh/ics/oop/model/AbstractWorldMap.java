4c724418bdb1bbf2317b08592d9c908da89f99ab
Michał Dydek <dydmichal@gmail.com>
2023-11-28 14:41:41 +0100

    lab6

--- oolab/src/main/java/agh/ics/oop/model/AbstractWorldMap.java
+++ oolab/src/main/java/agh/ics/oop/model/AbstractWorldMap.java
@@ -5,10 +5,11 @@ import agh.ics.oop.model.util.MapVisualizer;
 
 import java.util.*;
 
-public abstract class AbstractWorldMap {
+public abstract class AbstractWorldMap{
     protected Map<Vector2d, Animal> animalMap = new HashMap<>();
     protected Boundary mapBounds;
     protected List<MapChangeListener> observatorList = new ArrayList<>();
+    private final MapVisualizer mapVisualizer = new MapVisualizer(getWorldMap());
 
     public void registerObservator(MapChangeListener observator){
         observatorList.add(observator);
@@ -40,7 +41,7 @@ public abstract class AbstractWorldMap {
         return animalMap.get(position);
     }
 
-    public void move(Animal animal, MoveDirection direction, MoveValidator moveValidator){
+    protected void move(Animal animal, MoveDirection direction, MoveValidator moveValidator){
         Vector2d positionPrev = animal.getPosition();
         animalMap.remove(animal.getPosition());
         animal.move(direction, moveValidator);
@@ -64,6 +65,6 @@ public abstract class AbstractWorldMap {
 
     @Override
     public String toString(){
-        return new MapVisualizer(getWorldMap()).draw(getCurrentBounds().lowerLeft(), getCurrentBounds().upperRight());
+        return mapVisualizer.draw(getCurrentBounds().lowerLeft(), getCurrentBounds().upperRight());
     }
 }
